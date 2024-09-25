1b853810e646c37721548e6b0962d1e90d833a78
Jorge Arias <jariasmartin@safareyes.es>
2024-09-25 20:11:36 +0200

    ♻️ Create a test for clientes

--- src/test/java/controllers/modazluzropa/ModazLuzRopaApplicationTests.java
+++ src/test/java/controllers/modazluzropa/ModazLuzRopaApplicationTests.java
@@ -1,13 +1,22 @@
 package controllers.modazluzropa;
 
+import controllers.modazluzropa.models.Cliente;
+import controllers.modazluzropa.repositories.ClienteRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ModazLuzRopaApplicationTests {
 
+    @Autowired
+    private ClienteRepository clienteRepositorio;
+
     @Test
     void contextLoads() {
+    // Me muestra los clientes uno por uno en la consola
+        for (Cliente cliente : clienteRepositorio.findAll()) {
+            System.out.println(cliente);
+        }
     }
-
 }
