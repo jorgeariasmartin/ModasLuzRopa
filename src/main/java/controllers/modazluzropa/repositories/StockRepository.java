eee1ea2d90fada765e8e254edd6d4f5c7b5c3d8c
Jorge Arias <jariasmartin@safareyes.es>
2024-10-15 17:41:57 +0200

    ✨ Add findByProductoIdAndTalla method

--- src/main/java/controllers/modazluzropa/repositories/StockRepository.java
+++ src/main/java/controllers/modazluzropa/repositories/StockRepository.java
@@ -1,9 +1,12 @@
 package controllers.modazluzropa.repositories;
 
+import controllers.modazluzropa.enumns.TipoTalla;
 import controllers.modazluzropa.models.Stock;
+import controllers.modazluzropa.models.Talla;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Integer> {
+    Stock findByProductoIdAndTalla(Integer idProducto, Talla talla);
 }
