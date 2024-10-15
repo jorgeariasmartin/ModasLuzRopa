0aaf2a9def26d637f2710fa5da5a8ac2f8020bd0
Jorge Arias <jariasmartin@safareyes.es>
2024-10-15 17:42:16 +0200

    ✨ Add findByClienteID method

--- src/main/java/controllers/modazluzropa/repositories/VentasRepository.java
+++ src/main/java/controllers/modazluzropa/repositories/VentasRepository.java
@@ -1,9 +1,13 @@
 package controllers.modazluzropa.repositories;
 
+import controllers.modazluzropa.models.Cliente;
 import controllers.modazluzropa.models.Ventas;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VentasRepository extends JpaRepository<Ventas, Integer> {
+    List<Ventas> findByClienteId(int clienteId);
 }
